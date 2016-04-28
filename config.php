7c02c204d44c77e2c4faab72d7d58d6b95b8105c
sashko94 <sashko@templatemonster.me>
2016-04-28 12:40:10 +0300

    change config file

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
+
+
 <?php
 
 $dir    = str_replace('index.php', '', $_SERVER['SCRIPT_NAME']);
 if (strpos($_SERVER['REQUEST_URI'], 'index.php')) {
-	$path   = dirname("http://$_SERVER[HTTP_HOST]") . "$dir";
+    $path   = dirname("http://$_SERVER[HTTP_HOST]") . "$dir";
 } else {
-	$path   = "http://$_SERVER[HTTP_HOST]$dir";
+    $path   = "http://$_SERVER[HTTP_HOST]$dir";
 }
 
 // Variables
@@ -14,13 +16,15 @@ $doc_title = $product_name . " " . $product_desc;
 
 $lang = 'en';
 if (isset($_GET['lang'])) {
-	$lang = $_GET['lang'];
+    $lang = $_GET['lang'];
 }
 
-$section_param = 'introduction';
+$section_param = 'preparation';
 if (isset($_GET['section'])) {
-	$section_param = $_GET['section'];
+    $section_param = $_GET['section'];
 }
 if (isset($_GET['utm_campaign'])) {
-	header('Location: index.php');
+    header('Location: index.php');
 }
+
+
