c535e2b46b5da32c59ff385e9c8db69cacd9e514
Aleksei Patsurkovskiy <mnemon11k1@gmail.com>
2017-03-10 16:28:50 +0200

    UPD: RD Mailform
    UPD: RD Mailform

--- sections/template_customization/en/mailform-settings.php
+++ sections/template_customization/en/mailform-settings.php
@@ -2,6 +2,6 @@
 <p>    HTML Website Template uses a set of <b>RD Mailform</b> plugins  for implementing contact forms.
     You can find more detailed instructions about these plugins in <b><a href="index.php?lang=en&section=extensions#rd-mailform">Extensions > RD Mailform</a></b> section.</p>
 
-<p>In order to set up the email address to send the e-mails (sent with the help of <b>RD Mailform</b>) to,
-    please replace in <b>bat/rd-mailform.php</b> file the e-mail, which is specified in <b>$recipients</b> variable, with the needed recipient's email:</p>
-<pre><code>$recipients = 'your@email.com';</code></pre>
+<p>In order to set up the email address to send the e-mails (sent with the help of <b>RD Mailform</b>),
+    please replace in <b>js/script.js</b> file, in RD Mailform initialization section, e-mail, which is specified in <b>recipients</b> variable, with the needed recipient's email:</p>
+<pre><code>recipients = 'test@demolink.com';</code></pre>
--- sections/template_customization/ru/mailform-settings.php
+++ sections/template_customization/ru/mailform-settings.php
@@ -3,6 +3,7 @@
     Подробную информацию по данным плагинам вы можете найти в разделе <b><a href="../index.php?lang=ru&section=extensions#rd-mailform">Extensions > RD Mailform</a></b>.
 </p>
 
-<p>Для того, чтобы указать email адрес, на который будут приходить сообщения, отправляемые с помощью <b>RD Mailform</b> необходимо в файле <b>bat/rd-mailform.php</b> вашего сайта заменить email, указанный в переменной <b>$recipients</b>, на соответствующий email  получателя.
+<p>Для того, чтобы указать email адрес, на который будут приходить сообщения, отправляемые с помощью
+  <b>RD Mailform</b> необходимо в файле <b>js/script.js</b> вашего сайта, в секции с инициализацией RD Mailform заменить email, указанный в переменной <b>recipients</b>, на соответствующий email  получателя.
 </p>
-<pre><code>$recipients = 'your@email.com';</code></pre>
+<pre><code>recipients = 'test@demolink.com';</code></pre>
