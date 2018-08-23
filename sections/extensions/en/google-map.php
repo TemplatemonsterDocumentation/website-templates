01cfaef51fef21f74a10616536383c6837df7e3d
atom <atom@templatemonster.me>
2018-08-23 17:17:10 +0300

    UPD: map doc

--- sections/extensions/en/google-map.php
+++ sections/extensions/en/google-map.php
@@ -1,204 +1,84 @@
-<h3>RD Google Maps</h3>
-<h6>HTML Website Template uses <b>RD Google Maps</b> to implement Google maps.
-  </h6>
+<h3>Google Maps</h3>
+
 <p>The plugin supports the following settings:</p>
 <ul class="marked-list">
-    <li>Changing map coordinates;</li>
-    <li>Adding custom map markers;</li>
-    <li>Adding pop-up windows for the markers;</li>
-    <li>Custom map styling.</li>
-    <li>Search on the map;</li>
+	<li>Google API key setup for the map;</li>
+	<li>Changing the map scale;</li>
+	<li>Modifying the map&rsquo;s center according to an address or coordinates;</li>
+	<li>Setting the map style;</li>
+	<li>Adding markers to the map.</li>
 </ul>
 
-<h4>How to add search on the map?</h4>
-<p>RD Google Maps supports adding search on the map. </p>
-<p>To enable search on the map, add a textfield with <b>id</b> <b>"rd-google-map-address"</b> and a button with <b>id</b> <b>“rd-google-map-address-submit”</b>.</p>
-<p>The fool code of the map with search will look the following way:</p>
+<h4>Google API key setup for the map</h4>
+<p>Without a key, your map will work in demo mode. You can find a guide on getting the key on <a href='https://developers.google.com/maps/documentation/javascript/get-api-key'>developers.google.com</a></p>
+<p>Insert the received key into <b>data-key</b> attribute:</p>
 
 <pre><code>
-    &lt;!--Google map search--&gt;
-    &lt;div class="form-wrap"&gt;
-      &lt;label for="rd-google-map-address" class="form-label rd-input-label"&gt;Address&lt;/label&gt;
-      &lt;div class="input-group"&gt;
-        &lt;input id="rd-google-map-address"
-                  type="text" name="address"
-                  data-constraints="@Required"
-                  placeholder="" autocomplete="off"
-                  class="form-input
-                  form-input-has-validation"&gt;
-          &lt;span class="input-group-btn"&gt;
-          &lt;a id="rd-google-map-address-submit" href="#" class="btn btn-primary"&gt;Find Address&lt;/a&gt;
-          &lt;/span&gt;
-      &lt;/div&gt;
-    &lt;/div&gt;
-    &lt;!-- RD Google Map--&gt;
-    &lt;div class="rd-google-map"&gt;
-      &lt;div id="rd-google-map"
-              data-zoom="15"
-              data-x="-73.9874068"
-              data-y="40.643180"
-              data-styles="[]"
-              class="rd-google-map__model"&gt;
-      &lt;/div&gt;
-      &lt;ul class="map_locations"&gt;
-        &lt;li data-x="-73.9874068" data-y="40.643180"&gt;
-          &lt;p&gt;9870 St Vincent Place, Glasgow, DC 45 Fr 45.&lt;/p&gt;
-        &lt;/li&gt;
-      &lt;/ul&gt;
-    &lt;/div&gt;
-  </code></pre>
+&lt;!-- Google Map--&gt;
+&lt;div class="google-map-container" data-key="YOUR_API_KEY"&gt;
+    &lt;div class="google-map"&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre>
 
-
-<h4>Changing map coordinates</h4>
-
-<p>To change map coordinates to your custom ones, specify 2 attributes in .html file in the corresponding section:</p>
+<h4>How to change the map center coordinates</h4>
+<p>To replace the map center coordinates with the necessary ones, you need: </p>
 
 <pre><code>
-        &lt;!-- RD Google Map--&gt;
-        &lt;div id="google-map" class="map_model"&gt;
-            ...
-        &lt;/div&gt;
-    </code></pre>
+&lt;!-- Google Map--&gt;
+&lt;div class="google-map-container"&gt;
+    &lt;div class="google-map"&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre>
 
-<p><b>data-x</b> (northern longitude) and <b>data-y</b> (western latitude). As a result, you'll get the following lines of code:</p>
-
-<pre><code>
-        &lt;!-- RD Google Map--&gt;
-        &lt;div class="rd-google-map"&gt;
-            &lt;div id="google-map" class="map_model" data-x="-73.9994068" data-y="40.643180"&gt;
-                ...
-            &lt;/div&gt;
-            ...
-        &lt;/div&gt;
-    </code></pre>
-<p>You can find out the coordinates with the help of Google Maps service. To do this, right-click the target map sector and select the "What's there?" option.
-  In the pop-up window, you 'll the needed to copy coordinates below the location name, where the first number is <b>data-x</b> and the second one is <b>data-y</b>.</p>
-<p class="alert alert-warning">Attention: it is often the case, that the coordinates get mixed. Then, the map will display a wrong address in the opposite hemisphere.</p>
-
-<h4>How to add a map marker</h4>
-<p>To add a custom map marker, add a new element to the list in the .html file:</p>
-<pre><code>
-        &lt;!-- RD Google Map--&gt;
-        ...
-        &lt;ul class="map_locations"&gt;
-            ...
-        &lt;/ul&gt;
-        ...
-    </code></pre>
-
-<p>and define two data attributes in it: <b>data-x</b> (longitude) and <b>data-y</b> (latitude). As a result, you’ll get:</p>
+<p>to replace a required address in <b>data-center</b> attribute in .html file with a map in the section. As a result, you will get the following code:</p>
 
 <pre><code>
-        &lt;!-- RD Google Map--&gt;
-        ...
-        &lt;ul class="map_locations"&gt;
-            &lt;li data-x="-73.9994068" data-y="40.643180"&gt;&lt;/li&gt;
-        &lt;/ul&gt;
-        ...
-    </code></pre>
+&lt;!-- Google Map--&gt;
+&lt;div class="google-map-container" data-center="9870 St Vincent Place, Glasgow, DC 45 Fr 45."&gt;
+    &lt;div class="google-map"&gt;&lt;/div&gt;
+&lt;/div&gt;
+</code></pre>
 
-<p>If you do not specify the coordinates, the script will ignore an empty list item and the marker will not be displayed.</p>
+<h4>How to add a marker to the map</h4>
+<p>To add your marker to the map, you need to add ito the list:</p>
+<p>Для того чтобы добавить свой маркер на карту, необходимо в .html файле с картой в список:</p>
+<pre><code>
+&lt;!-- Google Map--&gt;
+...
+&lt;ul class="google-map-markers"&gt;
+    ...
+&lt;/ul&gt;
+...
+</code></pre>
 
-<p>The full code example looks the following way:</p>
+<p>new position with <b>data-location</b> attribute in .html file with the map. As a result, you will get:</p>
 
 <pre><code>
-        &lt;!-- RD Google Map--&gt;
-        &lt;div class="rd-google-map"&gt;
-            &lt;div id="google-map" class="map_model"&gt;&lt;/div&gt;
+&lt;!-- Google Map--&gt;
+...
+&lt;ul class="google-map-markers"&gt;
+    &lt;li data-location="9870 St Vincent Place, Glasgow, DC 45 Fr 45."&gt;&lt;/li&gt;
+&lt;/ul&gt;
+...
+</code></pre>
 
-            &lt;ul class="map_locations"&gt;
-                &lt;li data-x="-73.9874068" data-y="40.643180"&gt;
-                    &lt;p&gt; 9870 St Vincent Place, Glasgow, DC 45 Fr 45.
-                        &lt;span&gt;800 2345-6789&lt;/span&gt;
-                    &lt;/p&gt;
-                &lt;/li&gt;
-                &lt;li data-x="-72.9874068" data-y="39.643180"&gt;
-                    &lt;p&gt; 9870 St Vincent Place, Glasgow, DC 45 Fr 45.
-                        &lt;span&gt;800 2345-6789&lt;/span&gt;
-                    &lt;/p&gt;
-                &lt;/li&gt;
-            &lt;/ul&gt;
-        &lt;/div&gt;
-    </code></pre>
-<p>where:</p>
+<h4>Adding a description to the marker</h4>
+<p>To add a popup window to a user marker, you need to specify content for the window in <b>data-description</b> attribute:</p>
 
 <pre><code>
-        &lt;!-- RD Google Map--&gt;
-        ...
-        &lt;li data-x="-73.9874068" data-y="40.643180"&gt;
-            &lt;p&gt; 9870 St Vincent Place, Glasgow, DC 45 Fr 45.
-                &lt;span&gt;800 2345-6789&lt;/span&gt;
-            &lt;/p&gt;
-        &lt;/li&gt;
-        ...
-    </code></pre>
-<p>is the new map marker.</p>
+&lt;!-- Google Map--&gt;
+...
+&lt;li data-location="9870 St Vincent Place, Glasgow, DC 45 Fr 45." data-description="9870 St Vincent Place, Glasgow"&gt;&lt;/li&gt;
+...
+</code></pre>
 
-<h4>Adding marker popup windows</h4>
-
-<p>To add a marker popup window, just add the pop-up window content to the corresponding item of the map markers list:</p>
+<h4>How to replace the map style</h4>
+<p>The map supports lots of styling variants. You can get different ready-made styling variations on the website: <a href="https://snazzymaps.com/">https://snazzymaps.com/.</a></p>
+<p>On the given website you need to copy the style array of a map you prefer and insert it into <b>data-styles</b> attribute of a corresponding map on a target *.html page:</p>
 
 <pre><code>
-        &lt;!-- RD Google Map--&gt;
-        ...
-        &lt;li data-x="-73.9994068" data-y="40.643180" &gt;
-            ...
-        &lt;/li&gt;
-        ...
-    </code></pre>
-
-<p>As a result, you'll get the following lines of code :</p>
-
-<pre><code>
-        &lt;!-- RD Google Map--&gt;
-        ...
-        &lt;li data-x="-73.9994068" data-y="40.643180" &gt;
-            Our office
-        &lt;/li&gt;
-        ...
-    </code></pre>
-
-<h4>Changing map styling</h4>
-<p>RD Google Map supports many map styling variants. Some ready-made styling solution can be found on the following website:<a href="https://snazzymaps.com/">https://snazzymaps.com/.</a></p>
-
-<p>On this website, copy the set of styles you like to the <b>'data-style'</b> attribute of the map on the target *.html page.</p>
-<pre><code>
-        &lt;!-- RD Google Map--&gt;
-        &lt;div id="google-map" class="map_model" data-styles='Your Map Styles'&gt;
-            ...
-        &lt;/div&gt;
-    </code></pre>
-
-<h4>How to add search on the map</h4>
-<p>RD Google Map supports enabling search on the map.</p>
-<p>To enable search on the map, add a textfield with <strong>"rd-google-map-address"</strong> id and a button with <strong>rd-google-map-address-submit</strong> id.</p>
-<pre>
-  <code>
-    &lt;!-- RD Google Map Search--&gt;
-    &lt;input type="text" id="rd-google-map-address"&gt;
-    &lt;a href="#" class="btn btn-primary" id="rd-google-map-address-submit"&gt;
-    &lt;/div&gt;
-  </code>
-</pre>
-
-<p>The full sample code of the map with search should look the following way:</p>
-<pre>
-  <code>
-    &lt;!-- RD Google Map With Search--&gt;
-    &lt;div class="form-wrap"&gt;
-    &lt;label for="rd-google-map-address" class="form-label rd-input-label"&gt;Address&lt;/label&gt;
-    &lt;div class="input-group"&gt;
-    &lt;input id="rd-google-map-address" type="text" name="address" autocomplete="off" class="form-input"/&gt;
-    &lt;span class="input-group-btn"&gt;
-    &lt;a id="rd-google-map-address-submit" href="#" class="btn btn-primary"&gt;Find Address&lt;/a&gt;
-    &lt;/span&gt;
-    &lt;/div&gt;
-    &lt;/div&gt;
-    &lt;!-- RD Google Map--&gt;
-    &lt;div class="rd-google-map"&gt;
-    &lt;div id="google-map" data-zoom="14" data-x="-73.9874068" data-y="40.643180"&gt;&lt;/div&gt;
-    &lt;/div&gt;
-
-  </code>
-</pre>
-
+&lt;!-- Google Map--&gt;
+&lt;div class="google-map-container" data-styles='Your Map Styles'&gt;
+    ...
+&lt;/div&gt;
+</code></pre>
